b1989e4d237f7aefbc43a96043ca6411042f7519
plankton12345 <meroeve1224@gmail.com>
2017-02-24 21:15:59 +0900

    updata
    updata

--- src/src/plugin/plugin.php
+++ src/src/plugin/plugin.php
@@ -5,9 +5,6 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\command\CommandExecutor;
 use pocketmine\plugin\PluginBase;
-use ClassLoader;
-use pocketmine\Player;
-use pocketmine\Server;
 use pocketmine\event\Listener;
 use pocketmine\utils\Config;
 class Plugin extends PluginBase implements Listener{
@@ -19,24 +16,23 @@ function onEnable(){
 	}
     
 public function onCommand(CommandSender $sender, Command $command, $label, array $args){
-if($command->getName() == "join"){
-    
-		if($this->config->exists($sender)){
+	if($command->getName() == "joingroup"){
+       	if($this->config->exists($sender)){
 			 $gr=$this->config->get($sender);
                          $sender->sendMessage("あなたは"."$gr"."に参加しています");  
 		}else{
                     if($this->config->exists($label)){
                         $this->config->set("$sender", "$label");
                         $this->config->save();
-                        $sender->sendMessage("$label"."に参加しました");
+                        $sender->sendMessage($label."に参加しました");
                     }
                     }
-                }
-if($command->getName() == "creategroup"){
-if($this->config->exists("$label")){
+        }
+        if($command->getName() == "creategroup"){
+        if($this->config->exists("$label")){
           $sender->sendMessage("その名前のGroupはすでに存在します");
           }else{
-$this->config->set("$label", "$label");
+$this->config->set($label.$label);
 $this->config->save();
 $sender->sendMessage("Groupを作成しました");
             }
